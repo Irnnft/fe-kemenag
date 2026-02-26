786d8e5a458dfe10bc8c0c4102a89512f58107fb
Irnnft <irfantoni2005@gmail.com>
2026-02-26 11:08:01 +0700

    uhuy

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api';
 
 export const apiFetch = async (endpoint: string, options: RequestInit = {}) => {
     const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
